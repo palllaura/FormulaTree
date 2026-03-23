4d0b6a72aed60e8123b4aebd2afb25d4cc92fad0
palllaura <palllaura.tln@gmail.com>
2026-03-23 13:32:49 +0200

    Initial commit: project setup with spring boot, postgres, docker and base configuration

---
+++ src/main/java/com/palllaura/formulatree/FormulatreeApplication.java
@@ -0,0 +1,13 @@
+package com.palllaura.formulatree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormulatreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormulatreeApplication.class, args);
+	}
+
+}
