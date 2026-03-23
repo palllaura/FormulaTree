02f70d091481f4ff99d0c422a7e999fb06920f52
palllaura <palllaura.tln@gmail.com>
2026-03-23 15:36:11 +0200

    Add CarOption entity

---
+++ src/main/java/com/palllaura/formulatree/entity/CarOption.java
@@ -0,0 +1,61 @@
+package com.palllaura.formulatree.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "car_option")
+@Getter
+@Setter
+public class CarOption {
+
+    /**
+     * Unique car option ID.
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    /**
+     * Car option name.
+     */
+    @Column(nullable = false)
+    private String name;
+
+    /**
+     * Hierarchy level (0 = brand, 1 = series, 2 = model).
+     */
+    @Column(nullable = false)
+    private Integer level;
+
+    /**
+     * Parent option for hierarchy (optional).
+     */
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    @JsonIgnore
+    private CarOption parent;
+
+
+    /**
+     * List with relevant children options.
+     */
+    @OneToMany(mappedBy = "parent", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CarOption> children = new ArrayList<>();
+
+}
