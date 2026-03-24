e6d4d5dcc8fb140e302ddf86bec31bb4a450f933
palllaura <palllaura.tln@gmail.com>
2026-03-24 11:59:13 +0200

    Add data loader integration tests

---
+++ src/test/java/com/palllaura/formulatree/service/CarOptionDataLoaderIntegrationTest.java
@@ -0,0 +1,84 @@
+package com.palllaura.formulatree.service;
+
+import com.palllaura.formulatree.entity.CarOption;
+import com.palllaura.formulatree.repository.CarOptionRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+class CarOptionDataLoaderIntegrationTest {
+
+    @Autowired
+    private CarOptionRepository repository;
+
+    @Autowired
+    private CarOptionDataLoader loader;
+
+    @BeforeEach
+    void setUp() {
+        repository.deleteAll();
+        loader.loadIfEmpty();
+    }
+
+    @Test
+    void shouldLoadDataIntoDatabase() {
+        assertThat(repository.count()).isGreaterThan(0);
+    }
+
+    @Test
+    void shouldBuildCorrectHierarchy() {
+        List<CarOption> all = repository.findAll();
+
+        CarOption bmw = all.stream()
+                .filter(o -> o.getName().equals("BMW"))
+                .findFirst()
+                .orElseThrow();
+
+        CarOption series3 = all.stream()
+                .filter(o -> o.getName().equals("3 seeria"))
+                .findFirst()
+                .orElseThrow();
+
+        assertThat(series3.getParent()).isEqualTo(bmw);
+    }
+
+    @Test
+    void shouldCalculateLevelsCorrectly() {
+        List<CarOption> all = repository.findAll();
+
+        CarOption bmw = all.stream()
+                .filter(o -> o.getName().equals("BMW"))
+                .findFirst()
+                .orElseThrow();
+
+        CarOption series3 = all.stream()
+                .filter(o -> o.getName().equals("3 seeria"))
+                .findFirst()
+                .orElseThrow();
+
+        assertThat(bmw.getLevel()).isZero();
+        assertThat(series3.getLevel()).isEqualTo(1);
+    }
+
+    @Test
+    void shouldLoadThreeLevelHierarchyCorrectly() {
+        List<CarOption> all = repository.findAll();
+
+        CarOption model = all.stream()
+                .filter(o -> o.getName().equals("318"))
+                .findFirst()
+                .orElseThrow();
+
+        assertThat(model.getLevel()).isEqualTo(2);
+        assertThat(model.getParent()).isNotNull();
+        assertThat(model.getParent().getParent()).isNotNull();
+    }
+}
